48deddc00bdd00ca2fa98158321e0bf27cad0849
Emanuel Costa <emancos@gmail.com>
2022-09-10 09:10:49 -0300

    Filter date FindSale

--- backend/src/main/java/com/vendas/meta/controllers/SaleController.java
+++ backend/src/main/java/com/vendas/meta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.vendas.meta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vendas.meta.entities.Sale;
@@ -13,12 +14,17 @@ import com.vendas.meta.sevices.SaleService;
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
-	
+
 	@Autowired
 	private SaleService saleService;
-	
+
 	@GetMapping
-	public List<Sale> findSales() {
-		return saleService.findSale();
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable
+	) {
+
+		return saleService.findSale(minDate, maxDate, pageable);
 	}
 }
--- backend/src/main/java/com/vendas/meta/sevices/SaleService.java
+++ backend/src/main/java/com/vendas/meta/sevices/SaleService.java
@@ -1,8 +1,12 @@
 package com.vendas.meta.sevices;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.vendas.meta.entities.Sale;
@@ -14,7 +18,10 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 	
-	public List<Sale> findSale() {
-		return saleRepository.findAll();
+	public Page<Sale> findSale(String minDate, String maxDate,Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		return saleRepository.findSales(min, max, pageable);
 	}
 }
